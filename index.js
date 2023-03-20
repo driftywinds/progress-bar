400dbe781383c4d8b06d707df1de14d918c5d763
Lee Hadassin <lee@hadassin.com.au>
2023-03-20 21:58:23 +0800

    Fixed remaining ES5 compatibility issues

--- index.js
+++ index.js
@@ -2,27 +2,34 @@
 
 var UPDATES_PER_SECOND = 30;
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function () {
   var $title = $('#title');
   var $bar = $('#progress-bar');
   var $percentage = $('#progress-percentage');
 
-  setInterval(() => {
-    var { year, percentage } = getYearProgress();
+  setInterval(function () {
+    var _getYearProgress = getYearProgress(),
+      year = _getYearProgress.year,
+      percentage = _getYearProgress.percentage;
     $title.innerText = year;
-    $bar.style.setProperty('width', `${percentage}%`);
+    $bar.style.setProperty('width', "".concat(percentage, "%"));
     $percentage.innerText = percentage.toFixed(10);
   }, 1000 / UPDATES_PER_SECOND);
 });
 
 // Helpers
 
-var $ = (selector) => document.querySelector(selector);
+var $ = function $(selector) {
+  return document.querySelector(selector);
+};
 
 function getYearProgress() {
   var year = new Date().getFullYear();
-  var start = new Date(year,0,1,0,0,0,0);
-  var end = new Date(year+1,0,1,0,0,0,0);
+  var start = new Date(year, 0, 1, 0, 0, 0, 0);
+  var end = new Date(year + 1, 0, 1, 0, 0, 0, 0);
   var percentage = 100 * ((Date.now() - start) / (end - start));
-  return { year, percentage };
+  return {    
+    year: year,
+    percentage: percentage
+  };
 }
